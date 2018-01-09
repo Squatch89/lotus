2207faaa9ae9714b6914eee82d3a6a660fc91068
Gene Williams <GeneWillb@Gmail.com>
2018-01-09 18:10:24 -0500

    removed script to start node and react at same time to fix bugs

--- lotusreact/routes/htmlRoutes.js
+++ lotusreact/routes/htmlRoutes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const htmlRouter =  express.Router();
 
 htmlRouter.get("/home", function(req, res) {
-    res.send("woooo it works");
+    // res.send("woooo it works");
+    
+    res.json([{
+        id: 1,
+        username: "Gene"
+    }, {
+        id: 2,
+        username: "Julie"
+    }]);
 });
 
 module.exports =htmlRouter;
