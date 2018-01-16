27d4c854999135b385505d541792d8b6a40ecd98
Gene Williams <GeneWillb@Gmail.com>
2018-01-16 18:43:24 -0500

    updated master

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Jumbotron.css';
 import logo from "../Images/logo.png";
-import Header from '../Header/Header';
-import Footer from '../Footer/Footer';
+import Wrapper from '../Wrapper/Wrapper.js';
+import Container from '../Container/Container.js';
 
 // creates Jumbotron component to render to the  landing page
 const Jumbotron = () => {
     return (
-
-        <div className="wrapper">
-        <div className="container">
-        <div className="jumbotron text-center">
-            <img src={logo} alt="lotus logo" id="biglogo"/>
-            <h1>Lotus Tracker</h1>
-            <hr className="hr"/>
-            <p>Track your mental health, and take a break through meditation.</p>
-            <p className="lead">
-                <Link className="btn btn-primary btn-lg" to="/signin">Sign In</Link>
-            </p>
-        </div>
-        </div>
-        </div>
+        
+        <Wrapper>
+            <Container>
+                
+                <div className="jumbotron text-center">
+                    <img src={logo} alt="lotus logo" id="biglogo"/>
+                    <h1>Lotus Tracker</h1>
+                    <hr className="hr"/>
+                    <p>Track your mental health, and take a break through meditation.</p>
+                    <p className="lead">
+                        <Link className="btn btn-primary btn-lg" to="/signin">Sign In</Link>
+                    </p>
+                </div>
+            </Container>
+        </Wrapper>
+    
     )
 };
 
