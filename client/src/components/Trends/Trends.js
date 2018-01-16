3e712bc2131499049cf90798b901ce5f55c5bc2c
Gene Williams <GeneWillb@Gmail.com>
2018-01-16 16:00:39 -0500

    switching branches to implement charts on trends

--- client/src/components/Trends/Trends.js
+++ client/src/components/Trends/Trends.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import './Trends.css';
 // creates Trends component to render to the page
 const Trends = () => {
     return (
-        <div className="jumbotron text-center">
-            <h1>Trends</h1>
-            <hr className="hr"/>
-            <p>Track your mental health</p>
-            <p className="lead">
-                <Link className="btn btn-primary btn-lg" to="/">Home</Link>
-            </p>
+        <div>
+            <div className="jumbotron text-center">
+                <h1>Trends</h1>
+                <hr className="hr"/>
+                <p>Track your mental health</p>
+                <p className="lead">
+                    <Link className="btn btn-primary btn-lg" to="/">Home</Link>
+                </p>
+            </div>
         </div>
     )
 };
