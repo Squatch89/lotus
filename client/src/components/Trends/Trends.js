cea75f7de7b4a3ea27d0e718233eab0ab4122560
Gene Williams <GeneWillb@Gmail.com>
2018-01-22 14:26:10 -0500

    users can see previous week data now

--- client/src/components/Trends/Trends.js
+++ client/src/components/Trends/Trends.js
@@ -11,12 +11,20 @@ import moment from "moment";
 // creates Trends component to render to the page
 class Trends extends Component {
     constructor() {
+        const currentDate = new Date();
+        const currentDateMoment = moment(currentDate);
         super();
         this.state = {
             good: [],
             neutral: [],
             bad: [],
+            //user should select which time frame they would like to see
             timeFrame: ["Week", "Month"],
+            //set currentWeek to current week number using moment
+            currentWeek: currentDateMoment.week(),
+            //set currentMonth to current month number using moment
+            currentMonth: currentDateMoment.month() + 1,
+            prevWeek: currentDateMoment.week(),
             username: JSON.parse(sessionStorage.getItem('UN')),
             pulled: false
         };
@@ -31,13 +39,13 @@ class Trends extends Component {
                 console.log(data.data);
                 data.data.forEach((ele, index) => {
                     // console.log(ele.mood);
-                    if (ele.mood === "good") {
+                    if (ele.mood === "good" && moment(ele.date).week() === this.state.currentWeek) {
                         this.setState({good: [...this.state.good, ele.mood]})
                     }
-                    else if (ele.mood === "neutral") {
+                    else if (ele.mood === "neutral" && moment(ele.date).week() === this.state.currentWeek) {
                         this.setState({neutral: [...this.state.neutral, ele.mood]})
                     }
-                    else if (ele.mood === "bad") {
+                    else if (ele.mood === "bad" && moment(ele.date).week() === this.state.currentWeek) {
                         this.setState({bad: [...this.state.bad, ele.mood]})
                     }
                 });
@@ -59,34 +67,36 @@ class Trends extends Component {
     previousWeek = () => {
         //date format is in YYYY - MM - DD
         
-        const currentDate = new Date();
-        const currentDateMoment = moment(currentDate);
         
-        console.log(currentDateMoment.week());
-        console.log(currentDateMoment.date());
-        console.log(currentDateMoment.weekday());
-        console.log(currentDateMoment.month());
-        console.log(currentDateMoment.year());
-        console.log(`${currentDateMoment.year()}-${currentDateMoment.month()}-${currentDateMoment.date()}`);
+        //add if statement where if the current week matches the week in the data from the db, then display that info.
+        //week state will always be the current week number
+        //shadow variable will change based on how many times user clicks on previous week button
+        //each time user clicks on previous week button the shadow variable will decrement 1
+        //mimic this functionality to then return to current week
         
-        // console.log(currentDate.getDay());
-        // console.log(`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`);
-        
+        //implement week functionality into month function using same principles
+    
+        console.log(this.state.prevWeek);
+        if (this.state.prevWeek >= 0) {
+            this.setState({prevWeek: this.state.prevWeek - 1});
+        }
+        console.log(this.state.prevWeek);
         axios.get(`/api/mood/trends/prevweek/${this.state.username}`)
             .then((data) => {
                 console.log(data.data);
                 data.data.forEach((ele, index) => {
-                    let dbDate = new Date(ele.date);
-                    let dbDateMoment = moment(dbDate);
-                    console.log(dbDateMoment.week());
-                    console.log(dbDateMoment.date());
-                    // console.log(dbDateMoment.weekday());
-                    // console.log(dbDateMoment.month());
-                    // console.log(dbDateMoment.year());
-                    console.log(`${dbDateMoment.year()}-${dbDateMoment.month()}-${dbDateMoment.date()}`);
-                    // console.log(date.week());
-                    // console.log(date.getDay());
-                    // console.log(`${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`);
+                    
+                    console.log(`${moment(ele.date).year()}-${moment(ele.date).month() + 1}-${moment(ele.date).date()}`);
+    
+                    if (ele.mood === "good" && moment(ele.date).week() === this.state.prevWeek) {
+                        this.setState({good: [...this.state.good, ele.mood]})
+                    }
+                    else if (ele.mood === "neutral" && moment(ele.date).week() === this.state.prevWeek) {
+                        this.setState({neutral: [...this.state.neutral, ele.mood]})
+                    }
+                    else if (ele.mood === "bad" && moment(ele.date).week() === this.state.prevWeek) {
+                        this.setState({bad: [...this.state.bad, ele.mood]})
+                    }
                 })
             })
     };
